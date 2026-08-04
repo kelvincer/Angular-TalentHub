13699fadf18141d41872bb58ec249dd62354d57d
Kelvin <kelvin.cervan@entelgy.com>
2026-08-04 09:30:15 -0500

    login page develop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './features/auth/auth.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AuthComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    }
+];
---
+++ src/app/features/auth/auth.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-auth',
+  imports: [RouterLink],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css',
+})
+export class AuthComponent {}
